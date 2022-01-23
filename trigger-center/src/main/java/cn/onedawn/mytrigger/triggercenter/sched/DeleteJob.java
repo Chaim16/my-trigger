9f112251956343da45dc9ef3fb57d75e39c6fdd3
17665457395 <yqm.0229@qq.com>
2022-01-23 13:22:37 +0800

    接入 后台用户管理

--- trigger-center/src/main/java/cn/onedawn/mytrigger/triggercenter/sched/DeleteJob.java
+++ trigger-center/src/main/java/cn/onedawn/mytrigger/triggercenter/sched/DeleteJob.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author qingming yu
--- trigger-common/src/main/java/cn/onedawn/mytrigger/utils/ConstValue.java
+++ trigger-common/src/main/java/cn/onedawn/mytrigger/utils/ConstValue.java
@@ -12,13 +12,13 @@ public class ConstValue {
 
     public static final String REQUEST_DATA = "data";
 
-    public static final String SERVER = "192.168.4.1";
+    public static final String SERVER = "192.168.1.5";
     public static final String SERVER_PORT = "8080";
     public static final String BASE_URL = "http://" + ConstValue.SERVER + ":" + ConstValue.SERVER_PORT;
 
-    public static final String NAMESERVADDR = "http://182.92.233.6:9876";
+    public static final String NAMESERVADDR = "http://192.168.1.2:9876";
 
-    public static final String ZOOKEEPER_ADDRESS = "182.92.233.6:2181";
+    public static final String ZOOKEEPER_ADDRESS = "192.168.1.2:2181";
     public static final String ZOOKEEPER_USER = "root";
     public static final String ZOOKEEPER_PASSWORD = "123456";
 
